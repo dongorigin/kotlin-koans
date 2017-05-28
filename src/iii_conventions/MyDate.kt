d1a5a6764892153b569decf3ef27d8d70a22e848
dong <dongorigin@gmail.com>
2017-05-28 22:39:07 +0800

    completed kotlin koans 1 - 31

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +16,32 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class RepeatTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) = RepeatTimeInterval(this, number)
+
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+operator fun MyDate.plus(repeatTimeInterval: RepeatTimeInterval) = addTimeIntervals(repeatTimeInterval.timeInterval, repeatTimeInterval.number)
+
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    operator fun contains(d: MyDate): Boolean {
+        return d >= start && d <= endInclusive
+    }
+
+    override fun iterator(): Iterator<MyDate> = DateRangeIterator(this)
+}
+
+class DateRangeIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current: MyDate = dateRange.start
+
+    override fun hasNext(): Boolean {
+        return current <= dateRange.endInclusive
+    }
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+}
